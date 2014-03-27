aec48804d1ce91c1cf3c7f6d6e915ef37955bae1
Ioannis Kourouklides <leonariso@users.noreply.github.com>
2014-03-27 01:58:27 +0000

    Create ImmutableVsMutable.fs

---
+++ Samples/Basics/ImmutableVsMutable.fs
@@ -0,0 +1,8 @@
+// let bindings are immutable
+let x = 7 + 3
+let y = x + 1
+let z = x / 2
+
+//  mutable bindings
+let mutable modifiable = "original value"
+modifiable <- "new value" // NOTE: <- instead of =
