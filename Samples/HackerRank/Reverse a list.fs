69706f92ea80ccfb30548e7f4cc9a1abcf614490
Ioannis Kourouklides <kourouklides@users.noreply.github.com>
2015-06-21 02:33:25 +0100

    Update Reverse a list.fs

--- Samples/HackerRank/Reverse a list.fs
+++ Samples/HackerRank/Reverse a list.fs
@@ -15,3 +15,17 @@ let main argv =
     let reverse = inputList |> Seq.toArray |> Array.rev  |> Array.iter (printf "%d \n")
 
     0 // return an integer exit code
+(*
+open System
+let input = 
+    Seq.initInfinite (fun _ -> System.Console.ReadLine())
+    |> Seq.takeWhile (fun s -> s <> null)
+    |> Seq.map int
+    |> List.ofSeq
+[<EntryPoint>] 
+let main args = 
+  input
+  |> List.fold (fun acc n -> n::acc) List.empty
+  |> List.iter (fun n -> System.Console.WriteLine(n))
+  0
+*)
