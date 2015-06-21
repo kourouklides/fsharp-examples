e2c418d7928d8155d6cf0ca04dc1fefd42da24e1
Ioannis Kourouklides <kourouklides@users.noreply.github.com>
2015-06-21 01:02:41 +0100

    Update List_Replication.fs

--- Samples/HackerRank/List_Replication.fs
+++ Samples/HackerRank/List_Replication.fs
@@ -2,8 +2,6 @@ open System
 
 [<EntryPoint>]
 let main argv = 
-    // let s = Console.ReadLine() |> int
-
     let read _ = Console.ReadLine()
     let isValid = function null -> false | _ -> true
     let inputList =
@@ -11,8 +9,17 @@ let main argv =
         |> Seq.takeWhile isValid
         |> Seq.toList
         |> Seq.map int
+    
+    let s = inputList |> Seq.head
+    
+//    let s = Seq.nth 0 inputList
+    let s = inputList |> Seq.head
+    let L = inputList |> Seq.length
 
-    inputList |> Seq.iter (printf "%d \n")
-
-
+    for i = 1 to L-1 do
+        for j = 1 to s do
+            let x = Seq.nth i inputList 
+            printf "%d \n" x
+    
     0 // return an integer exit code
+    
