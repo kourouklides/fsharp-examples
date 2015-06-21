dc5d688f44daba3a7f69428c9847d0d1cd07e478
Ioannis Kourouklides <kourouklides@users.noreply.github.com>
2015-06-21 04:14:17 +0100

    Update Fibonacci Numbers.fs

--- Samples/HackerRank/Fibonacci Numbers.fs
+++ Samples/HackerRank/Fibonacci Numbers.fs
@@ -12,6 +12,17 @@ let main() =
     let input = Console.ReadLine() |> int
     let output = mutrec_fibonacci(input)
     printfn "%d" output
-
  
 main()
+
+(*
+open System
+
+let fibs = Seq.unfold (fun (a, b) -> Some(a, (b, a+b))) (0I, 1I)
+
+[<EntryPoint>]
+let main argv = 
+    let n = Console.ReadLine() |> int
+    printfn "%A" (Seq.nth (n-1) fibs)
+    0
+*)
