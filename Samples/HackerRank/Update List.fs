f875f71b2bb8cbad8510ccd7da62ce5600354e69
Ioannis Kourouklides <kourouklides@users.noreply.github.com>
2015-06-21 02:24:09 +0100

    Create Update List.fs

---
+++ Samples/HackerRank/Update List.fs
@@ -0,0 +1,14 @@
+let f arr = // Complete this function
+    arr
+   |> List.map (fun x -> abs(x) )
+
+//----------------DON'T MODIFY---------------
+
+let input = 
+    stdin.ReadToEnd().Split '\n' 
+    |> Array.map(fun x -> int(x)) 
+    |> Array.toList
+    
+let print_out (data:int list) = List.iter (fun x -> printfn "%A" x) data
+
+print_out (f(input))
