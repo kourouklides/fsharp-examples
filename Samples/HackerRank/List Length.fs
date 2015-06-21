02a520e865d6ca8825fd305b9fa39164d7b7d318
Ioannis Kourouklides <kourouklides@users.noreply.github.com>
2015-06-21 02:51:30 +0100

    Update List Length.fs

--- Samples/HackerRank/List Length.fs
+++ Samples/HackerRank/List Length.fs
@@ -15,4 +15,23 @@ let main argv =
     printfn "%d" L
     
     0 // return an integer exit code
+
+(*
+open System
+
+let countAll input = 
+     List.fold (fun acc x -> acc + 1) 0 input
+
+// Boiler plate stuff for HackerRank
+[<EntryPoint>]
+let main argv = 
     
+    let readInputsAsInts = 
+      Seq.initInfinite (fun _ -> System.Console.ReadLine())
+      |> Seq.takeWhile (fun s -> s <> null)
+      |> Seq.map(fun c -> int c)
+      |> Seq.toList
+    
+    Console.Write (countAll readInputsAsInts)
+    0
+*)
