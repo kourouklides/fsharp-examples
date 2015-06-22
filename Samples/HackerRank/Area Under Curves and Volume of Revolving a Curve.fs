2dc232a954433d18d5467fef4cb7a9ea604ed7cb
Ioannis Kourouklides <kourouklides@users.noreply.github.com>
2015-06-22 05:56:47 +0100

    Update Area Under Curves and Volume of Revolving a Curve.fs

--- Samples/HackerRank/Area Under Curves and Volume of Revolving a Curve.fs
+++ Samples/HackerRank/Area Under Curves and Volume of Revolving a Curve.fs
@@ -27,3 +27,32 @@ let main()=
     printfn "%f" V
 
 main()
+
+(*
+//Enter your code here. Read input from STDIN. Print output to STDOUT
+let toIntSeq (str : string) = (str.Split [|' '|]) |> Seq.map float
+let firstLine = System.Console.ReadLine()
+let secondLine = System.Console.ReadLine()
+let thirdLine = System.Console.ReadLine()
+
+let pol = Seq.zip (toIntSeq firstLine) (toIntSeq secondLine)
+let interval = (toIntSeq thirdLine) |> Seq.toList
+
+let eval p x =
+    p
+    |> Seq.map (fun (c,e) -> c * System.Math.Pow(x, e))
+    |> Seq.sum
+
+let area p a b step =
+    [a .. step .. b]
+    |> Seq.map (fun x -> (eval p x) * step)
+    |> Seq.sum
+
+let volume p a b step =
+    [a .. step .. b]
+    |> Seq.map (fun x -> System.Math.Pow(eval p x, 2.0) * System.Math.PI * step)
+    |> Seq.sum
+    
+printf "%.1f\n" (area pol interval.[0] interval.[1] 0.001)
+printf "%.1f\n" (volume pol interval.[0] interval.[1] 0.001)
+*)
